9c772adf2db18c33a10c267a4760e0fb59889d1f
Andrey Dorokhov <dorohov@smartdriving.io>
2018-12-10 19:28:40 +0300

    Add Register* methods

--- src/ExternalConfigurationProvider.Autofac/ContainerBuilderExtenstions.cs
+++ src/ExternalConfigurationProvider.Autofac/ContainerBuilderExtenstions.cs
@@ -10,6 +10,36 @@ namespace ExternalConfiguration.Autofac
         /// Configure Consul configuration provider services.
         /// Service configs will be cached. To avoid caching use overloaded method."/>
         /// </summary>
+        public static void RegisterConsulConfigurationProvider(this ContainerBuilder builder, string environment, Action<ConsulConfig> configuration)
+        {
+            RegisterConsulConfigurationProvider(builder, environment, true, configuration);
+        }
+
+        /// <summary>
+        /// Configure Consul configuration provider services.
+        /// </summary>
+        public static void RegisterConsulConfigurationProvider(this ContainerBuilder builder, string environment, bool useCache, Action<ConsulConfig> configuration)
+        {
+            if (string.IsNullOrEmpty(environment)) throw new ArgumentNullException(nameof(environment));
+
+            var config = new ConsulConfig();
+            configuration(config);
+
+            builder.Register(context => new ConsulConfigurationStore(config)).As<IExternalConfigurationStore>();
+
+            builder.Register(context => new ExternalConfiguration.ExternalConfigurationProvider(
+                    context.Resolve<IExternalConfigurationStore>(),
+                    environment,
+                    useCache))
+                .As<IExternalConfigurationProvider>()
+                .SingleInstance();
+        }
+
+        /// <summary>
+        /// Configure Consul configuration provider services.
+        /// Service configs will be cached. To avoid caching use overloaded method."/>
+        /// </summary>
+        [Obsolete("Will be deleted in 0.3.0")]
         public static void AddConsulConfigurationProvider(this ContainerBuilder builder, string environment, Action<ConsulConfig> configuration)
         {
             AddConsulConfigurationProvider(builder, environment, true, configuration);
@@ -18,6 +48,7 @@ namespace ExternalConfiguration.Autofac
         /// <summary>
         /// Configure Consul configuration provider services.
         /// </summary>
+        [Obsolete("Will be deleted in 0.3.0")]
         public static void AddConsulConfigurationProvider(this ContainerBuilder builder, string environment, bool useCache, Action<ConsulConfig> configuration)
         {
             if (string.IsNullOrEmpty(environment)) throw new ArgumentNullException(nameof(environment));
