7a20c9072977060bc8dd1b2b4dd06b2fb9192e0c
Andrey Dorokhov <dorohov@smartdriving.io>
2018-12-11 14:51:06 +0300

    Delete obsolete methods

--- src/ExternalConfigurationProvider.Autofac/ContainerBuilderExtensions.cs
+++ src/ExternalConfigurationProvider.Autofac/ContainerBuilderExtensions.cs
@@ -34,36 +34,5 @@ namespace ExternalConfiguration.Autofac
                 .As<IExternalConfigurationProvider>()
                 .SingleInstance();
         }
-
-        /// <summary>
-        /// Configure Consul configuration provider services.
-        /// Service configs will be cached. To avoid caching use overloaded method."/>
-        /// </summary>
-        [Obsolete("Will be deleted in 0.3.0")]
-        public static void AddConsulConfigurationProvider(this ContainerBuilder builder, string environment, Action<ConsulConfig> configuration)
-        {
-            AddConsulConfigurationProvider(builder, environment, true, configuration);
-        }
-
-        /// <summary>
-        /// Configure Consul configuration provider services.
-        /// </summary>
-        [Obsolete("Will be deleted in 0.3.0")]
-        public static void AddConsulConfigurationProvider(this ContainerBuilder builder, string environment, bool useCache, Action<ConsulConfig> configuration)
-        {
-            if (string.IsNullOrEmpty(environment)) throw new ArgumentNullException(nameof(environment));
-
-            var config = new ConsulConfig();
-            configuration(config);
-
-            builder.Register(context => new ConsulConfigurationStore(config)).As<IExternalConfigurationStore>();
-
-            builder.Register(context => new ExternalConfiguration.ExternalConfigurationProvider(
-                    context.Resolve<IExternalConfigurationStore>(),
-                    environment,
-                    useCache))
-                .As<IExternalConfigurationProvider>()
-                .SingleInstance();
-        }
     }
 }
