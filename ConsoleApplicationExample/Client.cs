71e4213c349d45f9914280a8c5fb028123967a14
Andrey Dorokhov <dorohov@smartdriving.io>
2018-12-11 12:19:52 +0300

    Add example project

---
+++ ConsoleApplicationExample/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationExample
+{
+    public class Client : IClient
+    {
+        private readonly string _url;
+
+        public Client(string url)
+        {
+            _url = url;
+        }
+
+        public Task SendRequestAsync()
+        {
+            Console.WriteLine($"Connecting to {_url}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ConsoleApplicationExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Autofac;
+using ExternalConfiguration;
+using ExternalConfiguration.Autofac;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApplicationExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            var containerBuilder = new ContainerBuilder();
+
+            var consulUrl = configuration.GetSection("Consul:Url").Value;
+            var consulToken = configuration.GetSection("Consul:Token").Value;
+            var environment = configuration.GetSection("Environment").Value;
+
+            // Previously you need to add to
+
+            containerBuilder.RegisterConsulConfigurationProvider(environment, config =>
+            {
+                config.Url = consulUrl;
+                config.Token = consulToken;
+            });
+
+            containerBuilder
+                .Register(c =>
+                {
+                    var configurationProvider = c.Resolve<IExternalConfigurationProvider>();
+
+                    var clientKey = configuration.GetSection("Consul:ClientKey").Value;
+                    var clientSettings = configurationProvider.GetServiceSettingsAsync(clientKey).Result;
+
+                    string url = null;
+                    clientSettings?.TryGetValue("url", out url);
+
+                    if (url == null) Console.WriteLine($"Consul KV storage doesn't contain value for '{clientKey}' key.");
+
+                    return new Client(url);
+                })
+                .As<IClient>();
+
+            var serviceProvder = containerBuilder.Build();
+
+            var client = serviceProvder.Resolve<IClient>();
+            await client.SendRequestAsync();
+            Console.ReadLine();
+        }
+    }
+}
